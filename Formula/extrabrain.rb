aa7c8cb4674c180c8b8e6fcd6ffb817972c640ba
Alexander Ross <alex@aross.se>
2019-05-18 17:20:24 +0200

    Update extrabrain

--- Formula/extrabrain.rb
+++ Formula/extrabrain.rb
@@ -1,7 +1,7 @@
 class Extrabrain < Formula
   desc "A CLI for https://extrabrain.se"
   homepage "https://extrabrain.se"
-  url "https://github.com/standout/ExtraBrainCLI/archive/1.1.3.tar.gz"
+  url "https://github.com/standout/ExtraBrainCLI/archive/1.1.4.tar.gz"
   sha256 "2036be5d689bc41aa13cc50ea7f556fc8d356845e5fc51a1d0741af5a2244ea2"
 
   head "https://github.com/standout/ExtraBrainCLI.git"
